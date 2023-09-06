96f04df96cc6a13a4070bfc7c06ecef17407a673
SuhasMarade <suhasmarade99@gmail.com>
2023-09-07 03:49:17 +0530

    Added routing

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,9 +1,12 @@
 import './Pokemon.css';
-function Pokemon ({name, image}) {
+import { Link } from 'react-router-dom';
+function Pokemon ({name, image, id}) {
     return (
         <div className='pokemon'>
-            <h3 className='pokemon-name'>{name}</h3>
-            <img className="pokemon-image" src={image} />
+            <Link to={`pokemon/${id}`}>
+              <h3 className='pokemon-name'>{name}</h3>
+              <img className="pokemon-image" src={image} />
+            </Link>
         </div>
     )
 
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -56,7 +56,7 @@ function PokemonList() {
 
             <div className="pokemon-wrapper">
               {(isLoading)? 'Loading...': 
-              pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id}/>)}
+              pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id}/>)}
             </div>
             <div className="controls">
               <button disabled={prevUrl == null} onClick={()=>setPokedexUrl(prevUrl)}>Prev</button>
